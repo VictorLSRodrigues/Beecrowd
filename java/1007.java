a151d724d1255253ff9e0b7054826cadf37389a0
VictorLSRodrigues <162334303+VictorLSRodrigues@users.noreply.github.com>
2025-02-18 18:08:05 -0300

    Create 1007.java

---
+++ java/1007.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	     int a = sc.nextInt();
+	     int b = sc.nextInt();
+	     int c = sc.nextInt();
+	     int d = sc.nextInt();
+	     int diferença = (a * b - c * d);
+	    
+		System.out.println(String.format("DIFERENCA = "+diferença));
+	}
+}
