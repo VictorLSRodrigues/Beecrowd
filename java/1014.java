abf9a40245ab8d3779a71705f7cdb61eb6598616
VictorLSRodrigues <162334303+VictorLSRodrigues@users.noreply.github.com>
2025-02-20 14:29:35 -0300

    Create 1014.java

---
+++ java/1014.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
+public class Main
+{
+	public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);
+	 DecimalFormat Df = new DecimalFormat();
+	  Df.applyPattern("0.000");
+	  double A = sc.nextDouble();
+	  double B = sc.nextDouble();
+	  double M = A / B;
+	  
+	  System.out.println(Df.format(M) + " km/l");
+	 
+	 
+	}
+}
